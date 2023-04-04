24c05df0f398e2f421a06a03556eb18f7ff3a6e0
2860754963 <2860754963@qq.com>
2023-04-04 15:03:02 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import '@/mobile/flexible' // 适配
+import '@/styles/reset.css' // 初始化样式
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
